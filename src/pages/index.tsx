f63ba8b79dd2522008995ca2d92dc62bb7cb46d8
henriqueteixeiradev <henriqueteixeira.dev@gmail.com>
2022-05-11 22:35:58 -0300

    startapp

---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+export default function Home() {
+  return (
+    <div
+      className={`
+        flex
+        h-10
+        w-full
+        items-center
+        justify-center
+        bg-violet-600
+    `}
+    >
+      <h1
+        className={`
+
+      `}
+      >
+        Hello World
+      </h1>
+    </div>
+  )
+}
